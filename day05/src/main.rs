fb1713a1c2336dbdf0b41c1d9a353a070fe9f857
Max Marze <max@marze.io>
2021-12-05 01:42:34 -0500

    Day 5

---
+++ day05/src/main.rs
@@ -0,0 +1,84 @@
+#[macro_use]
+extern crate scan_fmt;
+
+use std::collections::HashMap;
+
+use aoc_utils;
+use nalgebra::Vector2;
+
+fn part_one() {
+    if let Some(lines) = aoc_utils::read_input("./input.txt") {
+        let points: Vec<(Vector2<i64>, Vector2<i64>)> = lines
+            .iter()
+            .map(
+                |line| match scan_fmt!(line, "{d},{d} -> {d},{d}", i64, i64, i64, i64) {
+                    Ok((x1, y1, x2, y2)) => (Vector2::new(x1, y1), Vector2::new(x2, y2)),
+                    _ => panic!("oh no"),
+                },
+            )
+            .collect();
+
+        let mut overlaps: HashMap<Vector2<i64>, i64> = HashMap::new();
+
+        for (start, end) in points.iter() {
+            let d = end - start;
+            let dir = if d.y == 0 {
+                Vector2::new(d.x.signum(), 0)
+            } else if d.x == 0 {
+                Vector2::new(0, d.y.signum())
+            } else {
+                continue;
+            };
+
+            let mut s = *start;
+            while s != *end + dir {
+                overlaps.entry(s).and_modify(|v| *v += 1).or_insert(1);
+                s += dir;
+            }
+        }
+
+        let overlap_count = overlaps.iter().filter(|(_, v)| **v > 1).count();
+        println!("Overlaps {}", overlap_count);
+    }
+}
+
+fn part_two() {
+    if let Some(lines) = aoc_utils::read_input("./input.txt") {
+        let points: Vec<(Vector2<i64>, Vector2<i64>)> = lines
+            .iter()
+            .map(
+                |line| match scan_fmt!(line, "{d},{d} -> {d},{d}", i64, i64, i64, i64) {
+                    Ok((x1, y1, x2, y2)) => (Vector2::new(x1, y1), Vector2::new(x2, y2)),
+                    _ => panic!("oh no"),
+                },
+            )
+            .collect();
+
+        let mut overlaps: HashMap<Vector2<i64>, i64> = HashMap::new();
+
+        for (start, end) in points.iter() {
+            let d = end - start;
+            let dir = if d.y == 0 {
+                Vector2::new(d.x.signum(), 0)
+            } else if d.x == 0 {
+                Vector2::new(0, d.y.signum())
+            } else {
+                Vector2::new(d.x.signum(), d.y.signum())
+            };
+
+            let mut s = *start;
+            while s != *end + dir {
+                overlaps.entry(s).and_modify(|v| *v += 1).or_insert(1);
+                s += dir;
+            }
+        }
+
+        let overlap_count = overlaps.iter().filter(|(_, v)| **v > 1).count();
+        println!("Overlaps {}", overlap_count);
+    }
+}
+
+fn main() {
+    part_one();
+    part_two();
+}
